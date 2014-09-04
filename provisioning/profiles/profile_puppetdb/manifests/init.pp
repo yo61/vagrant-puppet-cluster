4452aaf9a3e1a612f87a7244630ae796e3c2e065
Robin Bowes <robin.bowes@yo61.com>
2014-09-04 14:03:32 +0100

    Make puppetdb listen on external IP

--- provisioning/profiles/profile_puppetdb/manifests/init.pp
+++ provisioning/profiles/profile_puppetdb/manifests/init.pp
@@ -2,9 +2,17 @@
 class profile_puppetdb {
 
   $postgresql_ip = hiera('postgresql_ip')
+  $puppetdb_ip = hiera('puppetdb_ip')
+  $puppetdb_name = hiera('puppetdb_name')
+  $puppetdb_user = hiera('puppetdb_user')
+  $puppetdb_pass = hiera('puppetdb_pass')
 
   class {'puppetdb::server':
-    database_host => $postgresql_ip
+    listen_address    => $puppetdb_ip,
+    database_host     => $postgresql_ip,
+    database_name     => $puppetdb_name,
+    database_username => $puppetdb_user,
+    database_password => $puppetdb_pass,
   }
 
 }
