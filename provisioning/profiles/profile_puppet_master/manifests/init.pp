b67455fef4a97a18de7c7cb8e846869711a78e46
Robin Bowes <robin.bowes@yo61.com>
2014-09-04 14:22:36 +0100

    Install puppet master and configure to use puppetdb

--- provisioning/profiles/profile_puppet_master/manifests/init.pp
+++ provisioning/profiles/profile_puppet_master/manifests/init.pp
@@ -1,5 +1,14 @@
 class profile_puppet_master{
 
+  $puppetdb_ip = hiera('puppetdb_ip')
+  $puppetdb_port = hiera('puppetdb_port')
+
+  class{'puppet::master':
+    environments          => 'directory',
+    storeconfigs          => true,
+    storeconfigs_dbserver => $puppetdb_ip,
+    storeconfigs_dbport   => $puppetdb_port,
+  }
 
 
 }
