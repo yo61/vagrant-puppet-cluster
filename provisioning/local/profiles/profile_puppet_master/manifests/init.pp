21be049eac529fc2e08104e43b703c27dc3a24f4
Robin Bowes <robin.bowes@yo61.com>
2014-09-05 13:47:02 +0100

    Commit all puppet master ssl changes

--- provisioning/local/profiles/profile_puppet_master/manifests/init.pp
+++ provisioning/local/profiles/profile_puppet_master/manifests/init.pp
@@ -3,14 +3,22 @@ class profile_puppet_master{
 
   $puppetdb_ip = hiera('puppetdb_ip')
   $puppetdb_port = hiera('puppetdb_port')
-  $certname = $::fqdn
+  $puppet_certname = hiera('puppet.certname')
   $ssldir = '/var/lib/puppet/ssl'
 
-  class{'puppet::master': }
+  class{'puppet::master':
+    environments          => 'directory',
+    storeconfigs          => true,
+    storeconfigs_dbserver => $puppetdb_ip,
+    storeconfigs_dbport   => $puppetdb_port,
+    generate_ssl_certs    => false,
+    certname              => $puppet_certname,
+  }
 
   class{'::profile_puppet_master::ssl':
-    certname     => $certname,
+    certname     => $puppet_certname,
     ssldir       => $ssldir,
-  }
+  }~>
+  Service['httpd']
 
 }
--- provisioning/local/profiles/profile_puppet_master/manifests/ssl.pp
+++ provisioning/local/profiles/profile_puppet_master/manifests/ssl.pp
@@ -18,79 +18,83 @@ class profile_puppet_master::ssl(
     group => $puppet_group,
   }
 
+
+
   file{
     $ssldir:
-      ensure => 'directory',
-      mode   => '0771';
+      ensure  => 'directory',
+      mode    => '0771';
     "${ssldir}/ca":
-      ensure => 'directory',
-      mode   => '0755';
+      ensure  => 'directory',
+      mode    => '0755';
     "${ssldir}/ca/ca_crl.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera('ssl.ca/ca_crl.pem'),
+      mode    => '0644';
     "${ssldir}/ca/ca_crt.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera('ssl.ca/ca_crt.pem'),
+      mode    => '0644';
     "${ssldir}/ca/ca_key.pem":
-      ensure => 'file',
-      mode   => '0640';
-    "${ssldir}/ca/ca_pub.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera('ssl.ca/ca_key.pem'),
+      mode    => '0640';
     "${ssldir}/ca/inventory.txt":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera('ssl.ca/inventory.txt'),
+      mode    => '0644';
     "${ssldir}/ca/private":
-      ensure => 'directory',
-      mode   => '0750';
+      ensure  => 'directory',
+      mode    => '0750';
     "${ssldir}/ca/private/ca.pass":
-      ensure => 'file',
-      mode   => '0640';
+      ensure  => 'file',
+      content => hiera('ssl.ca/private/ca.pass'),
+      mode    => '0640';
     "${ssldir}/ca/requests":
-      ensure => 'directory',
-      mode   => '0755';
+      ensure  => 'directory',
+      mode    => '0755';
     "${ssldir}/ca/serial":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera('ssl.ca/serial'),
+      mode    => '0644';
     "${ssldir}/ca/signed":
-      ensure => 'directory',
-      mode   => '0755';
+      ensure  => 'directory',
+      mode    => '0755';
     "${ssldir}/ca/signed/${certname}.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera("ssl.ca/signed/${certname}.pem"),
+      mode    => '0644';
     "${ssldir}/certificate_requests":
-      ensure => 'directory',
-      mode   => '0755';
-    "${ssldir}/certificate_requests/${certname}.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'directory',
+      mode    => '0755';
     "${ssldir}/certs":
-      ensure => 'directory',
-      mode   => '0755';
+      ensure  => 'directory',
+      mode    => '0755';
     "${ssldir}/certs/ca.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera('ssl.certs/ca.pem'),
+      mode    => '0644';
     "${ssldir}/certs/${certname}.pem":
-      ensure => 'file',
-      mode   => '0644';
-    "${ssldir}/crl.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera("ssl.certs/${certname}.pem"),
+      mode    => '0644';
     "${ssldir}/private":
-      ensure => 'directory',
-      mode   => '0750';
+      ensure  => 'directory',
+      mode    => '0750';
     "${ssldir}/private_keys":
-      ensure => 'directory',
-      mode   => '0750';
+      ensure  => 'directory',
+      mode    => '0750';
     "${ssldir}/private_keys/${certname}.pem":
-      ensure => 'file',
-      mode   => '0640';
+      ensure  => 'file',
+      content => hiera("ssl.private_keys/${certname}.pem"),
+      mode    => '0640';
     "${ssldir}/public_keys":
-      ensure => 'directory',
-      mode   => '0755';
+      ensure  => 'directory',
+      mode    => '0755';
     "${ssldir}/public_keys/${certname}.pem":
-      ensure => 'file',
-      mode   => '0644';
+      ensure  => 'file',
+      content => hiera("ssl.public_keys/${certname}.pem"),
+      mode    => '0644';
   }
 
 }
