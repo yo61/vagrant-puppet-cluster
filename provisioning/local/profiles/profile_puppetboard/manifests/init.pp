d4ffe0cb97187bfc386f05e9e8a95430207d6600
Robin Bowes <robin.bowes@yo61.com>
2014-09-08 18:51:48 +0100

    Deploy pboard cert to puppetboard node

--- provisioning/local/profiles/profile_puppetboard/manifests/init.pp
+++ provisioning/local/profiles/profile_puppetboard/manifests/init.pp
@@ -1,3 +1,47 @@
 class profile_puppetboard{
 
+  $puppetboard_certname = hiera('puppetboard.certname')
+  $ssl_key = hiera('puppetboard.ssl_key')
+  $ssl_cert = hiera('puppetboard.ssl_cert')
+  $port = hiera('puppetboard.http_port', 80)
+  $puppetdb_host = hiera('puppetdb.certname')
+  $puppetdb_port = hiera('puppetdb.ssl_list_port', 8081)
+
+  $ssl_dir = '/var/lib/puppet/ssl'
+  $ssl_key_path = "${ssl_dir}/private_keys/${puppetboard_certname}.pem"
+  $ssl_ca_cert_path = "${ssl_dir}/certs/ca.pem"
+  $ssl_cert_path = "${ssl_dir}/certs/${puppetboard_certname}.pem"
+
+  file{
+    $ssl_key_path:
+      ensure  => file,
+      content => $ssl_key,
+      owner   => 'puppetboard',
+      group   => 'puppetboard',
+      mode    => '0640';
+    $ssl_cert_path:
+      ensure  => file,
+      content => $ssl_cert,
+      owner   => 'puppetboard',
+      group   => 'puppetboard',
+      mode    => '0640';
+  }~>
+  class{'::apache':}
+  class{'::apache::mod::wsgi':
+    wsgi_socket_prefix => '/var/run/wsgi',
+  }
+  class{'::puppetboard':
+    manage_git          => true,
+    manage_virtualenv   => true,
+    puppetdb_host       => $puppetdb_host,
+    puppetdb_port       => $puppetdb_port,
+    puppetdb_key        => $ssl_key_path,
+    puppetdb_ssl_verify => $ssl_ca_cert_path,
+    puppetdb_cert       => $ssl_cert_path,
+  }
+  class{'::puppetboard::apache::vhost':
+    vhost_name => $puppetboard_certname,
+    port       => $port,
+  }
+
 }
